1b96ccff61cd80f841fab3cd93e02c597f66f234
AinTEAsports <benkirat.khalil@gmail.com>
2022-06-06 20:46:34 +0200

    listLimit.py : added geter with property

--- listLimit.py
+++ listLimit.py
@@ -9,8 +9,8 @@ class SizeError(Exception):
 
 @dataclass(kw_only=True)
 class LimitedList:
-    array: tuple[Any] = field(default_factory=tuple)
-    max_length: int
+    __array: tuple[Any] = field(default_factory=tuple)
+    __max_length: int
 
 
     def __post_init__(self) -> None :
@@ -24,19 +24,34 @@ class LimitedList:
             ValueError: if the max length is less than 0
         """
 
-        if self.max_length < 0:
+        if self.__max_length < 0:
             raise ValueError("You can't set max value to 0 or beneath")
         
-        if len(self.array) > self.max_length:
+        if len(self.__array) > self.__max_length:
             raise SizeError("The list is too long")
 
 
-    def setValue(self, index : int, newValue):
-        if index >= self.limit:
+    @property
+    def tuple_array(self) -> tuple[Any] :
+        return self.__array
+    
+    
+    @property
+    def list_array(self) -> list :
+        return list(self.__array)
+    
+    
+    @property
+    def __max_length(self) -> int :
+        return self.__max_length
+    
+
+    def set_value(self, index : int, new_value : Any) -> None :
+        if index >= self.__max_length:
             raise IndexError(f"Index ({index}) is not valid, this object limit is '{self.limit}'")
 
         tempList = list(self.__limitedList)
-        tempList[index] = newValue
+        tempList[index] = new_value
 
         self.__limitedList = tuple(tempList)
 
@@ -54,21 +69,8 @@ class LimitedList:
             return toReturn
 
 
-    def getTuple(self, showDefaultValue : bool = False) -> tuple :
-        if not showDefaultValue:
-            return [element for element in self.__limitedList if element != self.__defaultValue]
-        
-        return self.__limitedList
-        
-
-
-    def getLimit(self) -> int :
-        return self.limit
-
 
 if __name__ == "__main__":
     print(isinstance(None, object))
-
-
 
 # REVOIR L'ALGO DE TOUT CE BORDEL ET CHANGER SI BESOIN, IDEM POUR LES AUTRES FICHIERS
